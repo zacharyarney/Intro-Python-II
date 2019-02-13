349928c498c448e2ec8e3288e1e130ecb58ffca4
zacharyarney <zachary.arney@gmail.com>
2019-02-13 15:31:08 -0800

    adds  commented out __repr__ to Player
    I was using it in adv.py as the printed message to the user. I don' t know if that's a proper use for it so I just put all the same stuff in adv and commented it out in player

--- src/player.py
+++ src/player.py
@@ -5,3 +5,11 @@
 class Player:
     def __init__(self, location):
         self.location = location
+
+    # def __repr__(self):
+    #     if self.location == 'outside':
+    #         return f'You are outside of the Cave Entrance.\n{self.location.description}\n\n'
+    #     elif self.location == 'overlook':
+    #         return f'You are at the {self.location.name}.\n{self.location.description}\n\n'
+    #     else:
+    #         return f'You are in the {self.location.name}.\n{self.location.description}\n\n'
