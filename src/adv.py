1bd4c5b714c7f248a702b21e003c872afe944ad5
zacharyarney <zachary.arney@gmail.com>
2019-02-13 13:39:59 -0800

    quick format

--- src/adv.py
+++ src/adv.py
@@ -3,8 +3,7 @@ from room import Room
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+    'outside':  Room("Outside Cave Entrance", "North of you, the cave mount beckons"),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
