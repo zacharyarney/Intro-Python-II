2f12daf02193adcae88df1ea51fa399d149ef7eb
zacharyarney <zachary.arney@gmail.com>
2019-02-14 00:40:58 -0800

    formatting plus grammar args for rooms

--- src/adv.py
+++ src/adv.py
@@ -1,25 +1,28 @@
 from room import Room
 from player import Player
 from item import Item
+from animations import *
+from functions import clear
+from functions import input_error
 
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside The Cave Entrance", "North of you, the cave mount beckons."),
+    'outside':  Room('Outside The Cave Entrance', 'North of you, the cave mount beckons.', grammar='You are '),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+    'foyer':    Room('Foyer', '''Dim light filters in from the south. Dusty
+passages run north and east.'''),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+    'overlook': Room('Grand Overlook', '''A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.''', grammar='You are at the '),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+    'narrow':   Room('Narrow Passage', '''The narrow passage bends here from west
+to north. The smell of gold permeates the air.'''),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+    'treasure': Room('Treasure Chamber', '''You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.'''),
 }
 
 
