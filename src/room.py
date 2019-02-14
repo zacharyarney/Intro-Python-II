7f946e88501e2b623659c11a2407c1e9840e0947
zacharyarney <zachary.arney@gmail.com>
2019-02-14 00:37:32 -0800

    Adds get_info method to Room class

--- src/room.py
+++ src/room.py
@@ -3,7 +3,25 @@
 
 
 class Room:
-    def __init__(self, name, description, items=[]):
+    def __init__(self, name, description, grammar='You are in the ', items=[]):
         self.name = name
         self.description = description
         self.items = items
+        self.grammar = grammar
+
+        # directions
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None
+
+    def get_info(self):
+        print(f'{self.grammar}{self.name}.\n{self.description}\n')
+
+    # def __str__(self):
+    #     return f'{self.name}'
+
+    # def set_room(self, room, direction):
+    #     self[direction.lower()] = room 
+        
+    
