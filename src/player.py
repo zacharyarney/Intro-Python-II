b6fafc919803cde3944af70ba028bd8d606e0439
zacharyarney <zachary.arney@gmail.com>
2019-02-14 23:21:44 -0800

    updates function names in player

--- src/player.py
+++ src/player.py
@@ -16,7 +16,7 @@ class Player:
         drop = ['d', 'drop']
         items = self.location.items
         if len(items) < 1:
-            del_response('You find nothing.')
+            response('You find nothing.')
         else:
             item = items.pop(0)
             item.get_info()
@@ -25,13 +25,13 @@ class Player:
 [D]rop
 >>> ''').lower()
             if item_input in add:
-                del_response(f'{item.name} added to inventory.')
+                response(f'{item.name} added to inventory.')
                 self.add_item(item)
             elif item_input in drop:
-                del_response("Welp. Don't need that.")
+                response("Welp. Don't need that.")
                 items.append(item)
             else:
-                del_response("You can't do that with this item.")
+                response("You can't do that with this item.")
                 items.insert(0, item)
                 self.look()
 
@@ -51,5 +51,5 @@ class Player:
         if inv_input in back:
             self.get_location()
         else:
-            del_response('Huh?')
+            response('Huh?')
             self.get_inventory()
