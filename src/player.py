cc13f3e639bb40c5330ef11e9a3676d49a9e0d8e
zacharyarney <zachary.arney@gmail.com>
2019-02-14 23:08:35 -0800

    Adds look method to Player class to check rooms for items

--- src/player.py
+++ src/player.py
@@ -11,3 +11,27 @@ class Player:
     def get_location(self):
         slow_animation(walk)
         self.location.get_info()
+
+    def look(self):
+        add = ['a', 'add']
+        drop = ['d', 'drop']
+        items = self.location.items
+        if len(items) < 1:
+            del_response('You find nothing.')
+        else:
+            item = items.pop(0)
+            item.get_info()
+            item_input = input('''What do you want to do with this?
+[A]dd to inventory
+[D]rop
+>>> ''').lower()
+            if item_input in add:
+                del_response(f'{item.name} added to inventory.')
+                self.add_item(item)
+            elif item_input in drop:
+                del_response("Welp. Don't need that.")
+                items.append(item)
+            else:
+                del_response("You can't do that with this item.")
+                items.insert(0, item)
+                self.look()
