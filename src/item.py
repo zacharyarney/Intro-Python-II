fb7ee190c789367e0b0f0b511559eb0560282260
zacharyarney <zachary.arney@gmail.com>
2019-02-14 23:07:36 -0800

    Adds get_info method to Item class

--- src/item.py
+++ src/item.py
@@ -1,4 +1,9 @@
+from functions import *
+
 class Item:
     def __init__(self, name, description):
         self.name = name
         self.description = description
+
+    def get_info(self):
+        new_page(f'You found a {self.name}.\n{self.description}\n')
