2f82d9efbaca04ebe6e01c3f43fa74ea6744c391
zacharyarney <zachary.arney@gmail.com>
2019-02-14 23:10:31 -0800

    Adds inventory management to Player class--inventory attr, methods

--- src/player.py
+++ src/player.py
@@ -1,15 +1,14 @@
-from functions import clear
-from animations import *
+from functions import *
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 
 
 class Player:
-    def __init__(self, location):
+    def __init__(self, location, inventory=[]):
         self.location = location
+        self.inventory = inventory
 
     def get_location(self):
-        slow_animation(walk)
         self.location.get_info()
 
     def look(self):
@@ -35,3 +34,22 @@ class Player:
                 del_response("You can't do that with this item.")
                 items.insert(0, item)
                 self.look()
+
+    def add_item(self, item):
+        self.inventory.append(item)
+
+    def get_inventory(self):
+        back = ['b', 'back']
+        new_page('Inventory:')
+        if len(self.inventory) < 1:
+            print('You have nothing. Even your wallet is missing.\nHow did you lose that?')
+        else:
+            for i, item in enumerate(self.inventory):
+                print(f'{i + 1} - {item.name}')
+                
+        inv_input = input('\n[B]ack [U]se\n>>> ').lower()
+        if inv_input in back:
+            self.get_location()
+        else:
+            del_response('Huh?')
+            self.get_inventory()
